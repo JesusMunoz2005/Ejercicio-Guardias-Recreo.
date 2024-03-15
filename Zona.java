6d5af02a0dded1ef72e80fb6e46b9a2bbada8760
Iván Naranjo <145764905+IvanNaranjoEducaand@users.noreply.github.com>
2024-03-15 09:46:35 +0100

    Actualización Zona

--- Zona.java
+++ Zona.java
@@ -3,31 +3,44 @@ import java.util.HashMap;
 
 public class Zona {
 	private HashMap<Integer, String> relacionZona = new HashMap<Integer, String>();
+	private Integer numZona;
+	private String descZona;
 	private int numProfesores;
 	
-	public Zona(int zona) {
+	public Zona(Integer zona) {
+		this.relacionZona.put(1, "Zona: 1- Escaleras de entrada");
+		this.relacionZona.put(2, "Zona: 2- Pasillo Garita 1");
+		this.relacionZona.put(3, "Zona: 3- Patio de los Limones");
+		this.relacionZona.put(4, "Zona: 4- Pasillo entre edif 1 y 2");
+		this.relacionZona.put(5, "Zona: 5- Patio Naranjos");
+		
 		switch (zona) {
 		case 1:
-			this.relacionZona.put(1, "Zona: 1- Escaleras de entrada");
+			this.numZona = zona;
+			this.descZona = this.relacionZona.get(zona);
 			this.setNumProfesores(1);
 			break;
 		case 2:
-			this.relacionZona.put(2, "Zona: 2- Pasillo Garita 1");
+			this.numZona = zona;
+			this.descZona = this.relacionZona.get(zona);
 			this.setNumProfesores(1);
 			break;
 			
 		case 3:
-			this.relacionZona.put(2, "Zona: 3- Pasillo de los limones");
+			this.numZona = zona;
+			this.descZona = this.relacionZona.get(zona);
 			this.setNumProfesores(1);
 			break;
 			
 		case 4:
-			this.relacionZona.put(2, "Zona: 4- Pasillo entre edif 1 y 2");
+			this.numZona = zona;
+			this.descZona = this.relacionZona.get(zona);
 			this.setNumProfesores(1);
 			break;
 			
 		case 5:
-			this.relacionZona.put(2, "Zona: 3- Patio Naranjos");
+			this.numZona = zona;
+			this.descZona = this.relacionZona.get(zona);
 			this.setNumProfesores(2);
 			break;
 
@@ -40,6 +53,14 @@ public class Zona {
 	
 	
 	//GETTER Y SETTER
+	public String getDescripcionZona(){
+		return this.descZona;
+	}
+	
+	public int getNumZona() {
+		return this.numZona;
+	}
+	
 	public int getNumProfesores() {
 		return numProfesores;
 	}
