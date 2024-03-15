6d32318581086712cb786044bcba5c072599cdd8
Adones2005 <145769029+Adones2005@users.noreply.github.com>
2024-03-15 10:15:37 +0100

    Update Zona.java

--- Zona.java
+++ Zona.java
@@ -1,71 +1,55 @@
 package guardias.recreo;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Zona {
-	private HashMap<Integer, String> relacionZona = new HashMap<Integer, String>();
-	private Integer numZona;
-	private String descZona;
-	private int numProfesores;
-	
-	public Zona(Integer zona) {
-		this.relacionZona.put(1, "Zona: 1- Escaleras de entrada");
-		this.relacionZona.put(2, "Zona: 2- Pasillo Garita 1");
-		this.relacionZona.put(3, "Zona: 3- Patio de los Limones");
-		this.relacionZona.put(4, "Zona: 4- Pasillo entre edif 1 y 2");
-		this.relacionZona.put(5, "Zona: 5- Patio Naranjos");
-		
-		switch (zona) {
-		case 1:
-			this.numZona = zona;
-			this.descZona = this.relacionZona.get(zona);
-			this.setNumProfesores(1);
-			break;
-		case 2:
-			this.numZona = zona;
-			this.descZona = this.relacionZona.get(zona);
-			this.setNumProfesores(1);
-			break;
-			
-		case 3:
-			this.numZona = zona;
-			this.descZona = this.relacionZona.get(zona);
-			this.setNumProfesores(1);
-			break;
-			
-		case 4:
-			this.numZona = zona;
-			this.descZona = this.relacionZona.get(zona);
-			this.setNumProfesores(1);
-			break;
-			
-		case 5:
-			this.numZona = zona;
-			this.descZona = this.relacionZona.get(zona);
-			this.setNumProfesores(2);
-			break;
+    private static final Map<Integer, String> relacionZona = new HashMap<>();
+    private static final Map<Integer, Integer> profesoresPorZona = new HashMap<>();
+    private final int numZona;
+    private final String descZona;
+    private static boolean zonasCreadas = false;
 
-		default:
-			this.setNumProfesores(-1);
-			break;
-		}
-	}
+    public static void generarZonas() {
+    	zonasCreadas = true;
+    
+        relacionZona.put(1, "Zona: 1- Escaleras de entrada");
+        relacionZona.put(2, "Zona: 2- Pasillo Garita 1");
+        relacionZona.put(3, "Zona: 3- Patio de los Limones");
+        relacionZona.put(4, "Zona: 4- Pasillo entre edif 1 y 2");
+        relacionZona.put(5, "Zona: 5- Patio Naranjos");
 
-	
-	
-	//GETTER Y SETTER
-	public String getDescripcionZona(){
-		return this.descZona;
-	}
-	
-	public int getNumZona() {
-		return this.numZona;
-	}
-	
-	public int getNumProfesores() {
-		return numProfesores;
-	}
+        profesoresPorZona.put(1, 1);
+        profesoresPorZona.put(2, 1);
+        profesoresPorZona.put(3, 1);
+        profesoresPorZona.put(4, 1);
+        profesoresPorZona.put(5, 2);
+    }
 
-	public void setNumProfesores(int numProfesores) {
-		this.numProfesores = numProfesores;
-	}
+    public Zona(int zona) {
+    	
+    	if(!zonasCreadas) //CREA LOS MAPAS AL CREAR LA PRIMERA INSTANCIA
+    		Zona.generarZonas();
+    	
+        if (relacionZona.containsKey(zona)) {
+            this.numZona = zona;
+            this.descZona = relacionZona.get(zona);
+        } else {
+            this.numZona = -1;
+            this.descZona = "Zona no válida";
+        }
+    }
+
+    // GETTER
+    public String getDescripcionZona() {
+        return this.descZona;
+    }
+
+    public int getNumZona() {
+        return this.numZona;
+    }
+
+    public int getNumProfesores() {
+        return profesoresPorZona.getOrDefault(numZona, -1);
+    }
 }
